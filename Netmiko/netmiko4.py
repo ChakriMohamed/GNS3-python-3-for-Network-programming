12aaace5e853c07852865cc9d2801660e7689e35
CHAKRI Mohamed <chakrimra@gmail.com>
2021-04-15 15:32:52 +0200

    Update netmiko4.py

--- Netmiko/netmiko4.py
+++ Netmiko/netmiko4.py
@@ -3,35 +3,35 @@ from netmiko import ConnectHandler
 iosv_l2_s2 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.82',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
 iosv_l2_s3 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.83',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
 iosv_l2_s4 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.84',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
 iosv_l2_s5 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.85',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
 iosv_l2_s6 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.86',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
