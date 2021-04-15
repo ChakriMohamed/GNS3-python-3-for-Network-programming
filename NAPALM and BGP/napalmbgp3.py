4f7c06e8032d156506a3abed372f37a4aa0310f7
CHAKRI Mohamed <chakrimra@gmail.com>
2021-04-15 15:39:57 +0200

    Update napalmbgp3.py

--- NAPALM and BGP/napalmbgp3.py
+++ NAPALM and BGP/napalmbgp3.py
@@ -1,7 +1,7 @@
 import json
 from napalm import get_network_driver
 driver = get_network_driver('ios')
-iosv = driver('192.168.122.72', 'david', 'cisco')
+iosv = driver('192.168.122.72', 'mohamed', 'cisco')
 iosv.open()
 
 ios_output = iosv.get_bgp_neighbors()
