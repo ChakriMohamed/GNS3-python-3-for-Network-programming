7ba3dfdece6cf3158ad952097cace0cd55cb382a
CHAKRI Mohamed <chakrimra@gmail.com>
2021-04-15 15:41:01 +0200

    Update netmiko2 - single device with file of commands.py

--- Iteration - Netmiko scripts/netmiko2 - single device with file of commands.py
+++ Iteration - Netmiko scripts/netmiko2 - single device with file of commands.py
@@ -6,7 +6,7 @@ with open('commands_file') as f:
 ios_devices = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.72',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
