552fa4b4c84b6447da89bb3dbb7f8ec1bf5b4599
CHAKRI Mohamed <chakrimra@gmail.com>
2021-04-15 15:32:26 +0200

    Update netmiko3.py

--- Netmiko/netmiko3.py
+++ Netmiko/netmiko3.py
@@ -3,21 +3,21 @@ from netmiko import ConnectHandler
 iosv_l2_s4 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.84',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
 iosv_l2_s5 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.85',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
 iosv_l2_s6 = {
     'device_type': 'cisco_ios',
     'ip': '192.168.122.86',
-    'username': 'david',
+    'username': 'mohamed',
     'password': 'cisco',
 }
 
